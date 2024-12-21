236fe0005a8e97959b122178e911105b245b7e87
HuTuTu <122086569+HuTuTuOnO@users.noreply.github.com>
2024-12-21 12:10:12 +0800

    Update client.sh

--- Stream/client.sh
+++ Stream/client.sh
@@ -368,11 +368,11 @@ for software in "${proxy_soft[@]}"; do
   routes_file="${soft_config_dir[$software]}"
   case "$software" in
   "soga" | "soga-docker") 
-    echo "提示：正在自动生成soga配置文件"
+    echo "提示：正在自动生成 soga 配置文件"
     generate_soga_config "$routes_file"
     ;;
   "xrayr") 
-    echo "提示：正在自动生成XrayR配置文件"
+    echo "提示：正在自动生成 XrayR 配置文件"
     generate_xrayr_config "$routes_file" 
     ;;
   *) 
@@ -386,15 +386,15 @@ done
 for software in "${proxy_soft[@]}"; do
   case "$software" in
   "soga")
-    echo "提示：正在重启soga"
+    echo "提示：正在重启 soga"
     systemctl restart soga
     ;;
   "soga-docker")
-    echo "提示：正在重启soga(docker)"
+    echo "提示：正在重启 soga(docker)"
     docker ps --filter ancestor=vaxilu/soga --format "{{.ID}}" | xargs -r docker restart
     ;;
   "xrayr")
-    echo "提示：正在重启XrayR"
+    echo "提示：正在重启 XrayR"
     systemctl restart XrayR
     ;;
   *) 
